e0ac542a6f42c74b6b4d5df6c72de73201991150
BrianC <seahorse8789@gmail.com>
2020-12-16 15:43:00 -0800

    first working draft of hexgrid

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import * as ScreenOrientation from 'expo-screen-orientation';
-import Canvas from 'react-native-canvas';
-import BFS from './src/Components/BFS';
-import { WebView } from 'react-native-webview';
+//   const window = Dimensions.get("window");
+//   const screen = Dimensions.get("screen");
+//     const [dimensions, setDimensions] = useState({ window, screen });
 
+
+//import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import BFS from './src/Components/BFS';
 
 async function changeScreenOrientation() {
   await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
 }
 
 export default function App() {
-  
-  const window = Dimensions.get("window");
-  const screen = Dimensions.get("screen");
-    const [dimensions, setDimensions] = useState({ window, screen });
-  
-    console.log('dimensions.height',dimensions.window.height)
-    console.log('dimensions.width',dimensions.screen.width)
-  
+      
   useEffect(() => {
     changeScreenOrientation()
   }, []);
@@ -31,97 +26,73 @@ export default function App() {
       </View>
 
   );
-}
 
-const styles = StyleSheet.create({
-     container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: 'black',
-    borderWidth: 1,
-    borderStyle: "solid",
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'green'
-   
-  },
+    }
   
-  profiles: {
-        width: '100%',
-    height: '100%',
-    marginTop: 0,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-
-  profiles: {
-        flex: 2,
-    justifyContent: 'space-between',
-    backgroundColor: 'red',
-    width: '100%',
-    maxHeight: '10%',
-    marginTop: 40,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-
-  player1: {
-        backgroundColor: 'red',
-    width: '38%',
-    //display: 'flex',
-    alignContent: 'center',
-    paddingLeft: '1%',
-    paddingRight: '1%',
-    alignItems: 'center',
-  },
-
-  player2: {
-        backgroundColor: 'green',
-    width: '38%',
-    //display: 'flex',
-    alignContent: 'center',
-    paddingLeft: '1%',
-    paddingRight: '1%',
-    alignItems: 'center',
-  },
-
-  time: {
-        backgroundColor: 'blue',
-    width: '24%',
-    //display: 'flex',
-    alignContent: 'center',
-    paddingLeft: '1%',
-    paddingRight: '1%',
-    alignItems: 'center',
-  },
-
+  const styles = StyleSheet.create({
+    container: {
+   flex: 1,
+   justifyContent: "center",
+   alignItems: "center",
+   borderColor: 'black',
+   borderWidth: 1,
+   borderStyle: "solid",
+   width: '100%',
+   height: '100%',
+   backgroundColor: 'green'
   
+ },
+ 
+ profiles: {
+       width: '100%',
+   height: '100%',
+   marginTop: 0,
+   flexDirection: 'row',
+   alignItems: 'center',
+ },
+
+ profiles: {
+       flex: 2,
+   justifyContent: 'space-between',
+   backgroundColor: 'red',
+   width: '100%',
+   maxHeight: '10%',
+   marginTop: 40,
+   flexDirection: 'row',
+   flexWrap: 'wrap',
+ },
+
+ player1: {
+       backgroundColor: 'red',
+   width: '38%',
+   //display: 'flex',
+   alignContent: 'center',
+   paddingLeft: '1%',
+   paddingRight: '1%',
+   alignItems: 'center',
+ },
+
+ player2: {
+       backgroundColor: 'green',
+   width: '38%',
+   //display: 'flex',
+   alignContent: 'center',
+   paddingLeft: '1%',
+   paddingRight: '1%',
+   alignItems: 'center',
+ },
+
+ time: {
+       backgroundColor: 'blue',
+   width: '24%',
+   //display: 'flex',
+   alignContent: 'center',
+   paddingLeft: '1%',
+   paddingRight: '1%',
+   alignItems: 'center',
+ },
+
+ 
 
 
 });
-
-//===========================
-
-
-
-
-import { GLView } from 'expo-gl'
-import Expo2DContext from 'expo-2d-context';
-import React from 'react';
- class App extends React.Component {
-    render() {
-        return (
-          <GLView
-            style={{ flex: 1 }}
-            onContextCreate={this._onGLContextCreate}
-          />
-        );
-    }
-    _onGLContextCreate = (gl) => {
-        var ctx = new Expo2DContext(gl);
-        
-    }
-}
-
-export default App;
